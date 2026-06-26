df6808e03fdb8d61e9578bb75c9d2cd8d0e63bd6
pedoow1 <131590379+pedoow1@users.noreply.github.com>
2026-06-26 21:53:26 +0300

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -51,7 +51,7 @@ function AuthPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
       if (error) {
